f9ede89b4bf20c612f25aaa4a3c3de96377e3ab2
H X <397224323@qq.com>
2021-10-08 18:53:22 +0800

    add 最长上升子序列的个数。用数组实现hash原理

---
+++ algorithm/algorithm/acwing_799 最长连续不重复子序列的长度.cpp
@@ -0,0 +1,33 @@
+//
+//  acwing_799 最长连续不重复子序列的长度.cpp
+//  algorithm
+//
+//  Created by 韩霄 on 2021/10/8.
+//双指针。使用s【a[i]】 来判断出现的个数。
+//双指针的形式for +while
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int N = 1e5+10;
+int a[N],s[N];
+int main(){
+    int n;
+    int res =0;
+    cin>>n;
+    for(int i=0;i<n;i++) cin>>a[i];
+    
+    for(int i=0,j=0;i<n;i++){
+        s[a[i]] ++;
+        while(s[a[i]] > 1){
+            s[a[j]]--;
+            j++; 
+        }
+        res = max(res,i-j+1);
+    }
+    cout<<res<<endl;
+    
+    return 0;
+}
