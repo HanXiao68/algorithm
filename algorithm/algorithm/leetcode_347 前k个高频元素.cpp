36c1d0dffe6a98115182de5aae4dfe1a79014c99
H X <397224323@qq.com>
2021-10-20 00:13:18 +0800

    add modify

--- algorithm/algorithm/leetcode_347 前k个高频元素.cpp
+++ algorithm/algorithm/leetcode_347 前k个高频元素.cpp
@@ -1,26 +1,27 @@
+////
+////  leetcode_347 前k个高频元素.cpp
+////  algorithm
+////
+////  Created by 韩霄 on 2021/10/19.
+////
 //
-//  leetcode_347 前k个高频元素.cpp
-//  algorithm
+//class Solution {
+//public:
+//    static bool cmp(pair<int,int>& x,pair<int,int>&y){
+//        return x.second >y.second;
+//    }
+//    vector<int> topKFrequent(vector<int>& nums, int k) {
+//        vector<int> res;
 //
-//  Created by 韩霄 on 2021/10/19.
+//        unordered_map<int,int> um;
 //
-
-class Solution {
-public:
-    static bool cmp(pair<int,int>& x,pair<int,int>&y){
-        return x.second >y.second;
-    }
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> res;
-        
-        unordered_map<int,int> um;
-        
-        for(auto x:nums) um[x]++;
-       vector<pair<int,int>> a(um.begin(),um.end());
-       sort(a.begin(),a.end(),cmp);
-
-        for(int i=0;i<k;i++)
-            res.push_back(a[i].first);
-        return res;
-    }
-};
+//        for(auto x:nums) um[x]++;
+////定义了一个vector a，元素装的是 um 中的 PII类型元素。
+//       vector<pair<int,int>> a(um.begin(),um.end());
+//       sort(a.begin(),a.end(),cmp);
+//
+//        for(int i=0;i<k;i++)
+//            res.push_back(a[i].first);
+//        return res;
+//    }
+//};
