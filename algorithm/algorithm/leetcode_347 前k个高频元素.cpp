eb5786a54d640d2652a46dd9463a77723e3d63e9
H X <397224323@qq.com>
2021-10-19 18:05:02 +0800

    add  leetcode_347 前k个高频元素

---
+++ algorithm/algorithm/leetcode_347 前k个高频元素.cpp
@@ -0,0 +1,26 @@
+//
+//  leetcode_347 前k个高频元素.cpp
+//  algorithm
+//
+//  Created by 韩霄 on 2021/10/19.
+//
+
+class Solution {
+public:
+    static bool cmp(pair<int,int>& x,pair<int,int>&y){
+        return x.second >y.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        
+        unordered_map<int,int> um;
+        
+        for(auto x:nums) um[x]++;
+       vector<pair<int,int>> a(um.begin(),um.end());
+       sort(a.begin(),a.end(),cmp);
+
+        for(int i=0;i<k;i++)
+            res.push_back(a[i].first);
+        return res;
+    }
+};
