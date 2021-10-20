98cce03f4adc52ddd58f22db2682e3d7ac324b25
H X <397224323@qq.com>
2021-10-20 22:55:33 +0800

    add  '投票法'

--- algorithm/algorithm/jianzhi_39 数组中出现次数超过一半的数字（多数投票法）.cpp
+++ algorithm/algorithm/jianzhi_39 数组中出现次数超过一半的数字（多数投票法）.cpp
@@ -1,23 +1,23 @@
+////
+////  多数投票法）.cpp
+////  algorithm
+////
+////  Created by 韩霄 on 2021/10/19.
+////
 //
-//  多数投票法）.cpp
-//  algorithm
-//
-//  Created by 韩霄 on 2021/10/19.
-//
-
-class Solution {
-public:
-    int MoreThanHalfNum_Solution(vector<int> numbers) {
-    
-        int count=0;
-        int num = -1;
-        for(int i=0;i<numbers.size();i++){
-            if(num == numbers[i]) ++count;
-            else if(--count <0){
-                num = numbers[i];
-                count =1;
-            }
-        }
-        return num;
-    }
-};
+//class Solution {
+//public:
+//    int MoreThanHalfNum_Solution(vector<int> numbers) {
+//    
+//        int count=0;
+//        int num = -1;
+//        for(int i=0;i<numbers.size();i++){
+//            if(num == numbers[i]) ++count;
+//            else if(--count <0){
+//                num = numbers[i];
+//                count =1;
+//            }
+//        }
+//        return num;
+//    }
+//};
