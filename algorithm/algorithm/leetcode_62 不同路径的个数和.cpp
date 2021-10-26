ca536ac92774b1886d07c5c609f725ecf16d31a5
H X <397224323@qq.com>
2021-10-27 03:17:54 +0800

    add  leetcode_62 不同路径的个数和

---
+++ algorithm/algorithm/leetcode_62 不同路径的个数和.cpp
@@ -0,0 +1,23 @@
+//
+//  leetcode_62 不同路径的个数和.cpp
+//  algorithm
+//
+//  Created by 韩霄 on 2021/10/27.
+//
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m+1][n+1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j< n;j++){
+                if(i == 0 ||j ==0) dp[i][j] = 1;
+            else{
+                dp[i][j] =dp[i-1][j] +dp[i][j-1];
+            }
+            }
+        }
+        return dp[m-1][n-1];
+  }
+    
+};
