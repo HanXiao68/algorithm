1d49bc7e872c5286b1671633e8064ef611982b0a
H X <397224323@qq.com>
2021-10-27 10:09:23 +0800

    add   leetcode_76 最小覆盖子串

--- algorithm/algorithm/leetcode_76 最小覆盖子串.cpp
+++ algorithm/algorithm/leetcode_76 最小覆盖子串.cpp
@@ -9,8 +9,9 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         unordered_map<char,int> um,window;//统计个数的
+//        um代表模式串中的个数统计
         for(char c:t) um[c]++;
-
+//        双指针遍历窗口。相同方向。
         int left =0,right =0;
         int valid = 0;
 
@@ -23,13 +24,17 @@ public:
                 window[c]++;
                 if(window[c] == um[c]) valid++;
             }
+//            当窗口的内的字母包含了模式串的字母时。
             while(valid == um.size()){
+//                若有更小的len 更新len,start.
                 if(right - left <len){
                     start = left;
                     len = right - left;
                 }
                 char d = s[left];
-                left++;
+//                左指针往右移。
+                   left++;
+//                更新左移后的两个窗口大小以及匹配的大小。
                 if(um.count(d)){
                     if(window[d] ==um[d]) valid--;
                     window[d] --;
