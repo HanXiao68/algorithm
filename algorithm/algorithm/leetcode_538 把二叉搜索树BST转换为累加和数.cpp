c2c7c30fdbca531efe9eecefae6d0361c5928def
H X <397224323@qq.com>
2021-10-27 14:05:34 +0800

    add   leetcode_538 把二叉搜索树BST转换为累加和数

---
+++ algorithm/algorithm/leetcode_538 把二叉搜索树BST转换为累加和数.cpp
@@ -0,0 +1,33 @@
+//
+//  leetcode_538 把二叉搜索树BST转换为累加和数.cpp
+//  algorithm
+//
+//  Created by 韩霄 on 2021/10/27.
+//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        int sum = 0;
+
+    TreeNode* convertBST(TreeNode* root) {
+        if (root != nullptr) {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+        return root;
+    }
+    
+};
