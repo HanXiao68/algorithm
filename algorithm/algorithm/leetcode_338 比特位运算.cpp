27f0d91e9b7ec47f5af9f505cafc993e33733e79
H X <397224323@qq.com>
2021-10-27 13:04:14 +0800

    add  mod

--- algorithm/algorithm/leetcode_338 比特位运算.cpp
+++ algorithm/algorithm/leetcode_338 比特位运算.cpp
@@ -1,23 +1,35 @@
+////
+////  leetcode_338 比特位运算.cpp
+////  algorithm
+////
+////  Created by 韩霄 on 2021/10/27.
+////
 //
-//  leetcode_338 比特位运算.cpp
-//  algorithm
-//
-//  Created by 韩霄 on 2021/10/27.
-//
-
 class Solution {
 public:
     vector<int> countBits(int n) {
-          vector<int>  res(num+1);
-
-        res[0] = 0;
-        for(int i =1;i <=num;i++){
-            if(i & 1){
-                res[i] = res[i-1]+1;
-            }else{
-                res[i] = res[i>>1];
-            }
+          vector<int>  res(n+1);
+        for(int i =0;i<=n;i++){
+            int ans =0;
+            int temp = i;
+            while(temp >0) {temp=temp&(temp-1);ans++;}
+            res[i] = ans;
         }
         return res;
+
+
+    // vector<int> countBits(int n) {
+    //     vector<int> bits(n + 1);
+    //     for (int i = 0; i <= n; i++) {
+    //         int ones = 0;
+    //         int x = i;
+    //         while (x > 0) {
+    //             x &= (x - 1);
+    //             ones++;
+    //         }
+    //         bits[i] = ones;
+    //     }
+    //     return bits;
+    
     }
 };
