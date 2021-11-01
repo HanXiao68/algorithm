fb13e5318b77a7a5ca8e965e43b014e594d0d6f7
H X <397224323@qq.com>
2021-11-01 22:00:47 +0800

    add  完全背包问题。每一个元素可以无限个选。但是总和要小于当前的target

--- algorithm/algorithm/acwing_39 数组中和等于target的组合.cpp
+++ algorithm/algorithm/acwing_39 数组中和等于target的组合.cpp
@@ -1,34 +1,30 @@
+////
+////  acwing_39 数组中和等于target的组合.cpp
+////  algorithm
+////
+////  Created by 韩霄 on 2021/10/23.
+////
 //
-//  acwing_39 数组中和等于target的组合.cpp
-//  algorithm
-//
-//  Created by 韩霄 on 2021/10/23.
-//
-
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int> ans;
+    vector<int> temp;
     vector<vector<int>> combinationSum(vector<int>& a, int target) {
-        sort(a.begin(),a.end());
+        
         dfs(a,target,0);
         return res;
     }
-
+    // 完全背包问题
     void dfs(vector<int>& a, int target,int idx){
-        
-        if(target ==0) {
-            res.push_back(ans);
-            return;
-        }
-        if(idx == a.size()) return;
-
-        if(a[idx] <= target){
-            dfs(a,target,idx+1);
-            ans.push_back(a[idx]);
-            dfs(a,target-a[idx],idx);
-            ans.pop_back();
-        }
-
+       if(target ==0) {res.push_back(temp);return;}
+       if(idx==a.size()) return;
+       for(int i=0;a[idx]*i<=target;i++){
+           dfs(a,target-a[idx]*i,idx+1);
+           temp.push_back(a[idx]);
+       }
+       for(int i=0;a[idx]*i<=target;i++){
+           temp.pop_back();
+           
+       }
     }
 };
