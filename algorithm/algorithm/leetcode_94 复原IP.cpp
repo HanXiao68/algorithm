73926df70df0204b89480e0b8866fc9fae0159d8
H X <397224323@qq.com>
2021-11-01 22:22:29 +0800

    add  第二种写法

--- algorithm/algorithm/leetcode_94 复原IP.cpp
+++ algorithm/algorithm/leetcode_94 复原IP.cpp
@@ -47,3 +47,37 @@
 //};
 
 //};
+
+////第二种写法
+//class Solution {
+//public:
+//    vector<string> res;
+//    vector<string> restoreIpAddresses(string s) {
+//        if(s.size() ==0) return res;
+//        dfs(s,"",4);
+//        return res;
+//    }
+//
+//    void dfs(string s,string out,int k){
+//        if(k ==0){
+//            if(s.empty()) res.push_back(out);
+//            return;
+//        }
+//
+//            for(int i=1;i<=3;i++){
+//                if(i<=s.size() &&isvalid(s.substr(0,i))){
+//                    if(k==1) dfs(s.substr(i),out+s.substr(0,i),k-1);
+//                    else dfs(s.substr(i),out+s.substr(0,i)+'.',k-1);
+//                }
+//            }
+//
+//    }
+//    bool isvalid(string s){
+//        if(s.empty() ||s.size()>3 ||(s.size()>1 &&s[0]=='0')) return false;
+//        int num = 0;
+//        for(auto x:s){
+//            num =num*10 +x-'0';
+//        }
+//        return num >=0 &&num <=255;
+//    }
+//};
